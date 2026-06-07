228c74fdca6cd8b762989eb83e5d9959e421874c
fhufijf <fhufijf8672@gmail.com>
2026-06-07 20:29:07 +0200

    3D dazu machen

--- pages/Drei_D.py
+++ pages/Drei_D.py
@@ -1,16 +1,31 @@
 import streamlit as st
 from streamlit_extras.three_viewer import *
 
+st.title("3D Viewer")
+st.subheader("Hier kanst du 3D Objekte anschauen")
 
-# Using a public GLB model from the Khronos glTF sample models
-Ente = (
-    "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Assets/main/Models/Duck/glTF-Binary/Duck.glb"
-)
+st.divider()
 
-Maus = (
-    "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Assets/main/Models/Duck/glTF-Binary/Duck.glb"
-)
+with st.echo(code_location="below"):
+    # Using a public GLB model from the Khronos glTF sample models
+    Ente = (
+        "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Assets/main/Models/Duck/glTF-Binary/Duck.glb"
+    )
 
-model = st.segmented_control("", [Ente, Maus])
+    model = Ente
 
-three_viewer(model, height=400, key="basic_demo")
+    Maus = (
+        "https://raw.githubusercontent.com/fhufijf/streamlit-webseite/main/pages/maus.glb"
+    )
+
+    if st.button("Ente", type="primary", width = "stretch"):
+        model = Ente
+
+    if st.button("Maus", type="primary", width = "stretch"):
+        model = Maus
+
+    st.divider()
+
+    three_viewer(model, height=400, key="basic_demo")
+
+    st.divider()
