20975c999a32992a392fa1d5c3aced8a0a05e0a8
fhufijf <fhufijf8672@gmail.com>
2026-06-07 19:43:39 +0200

    Mit Codex die Files gross geschrieben

--- webseite/pages/DVD_video.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-dvd = "https://www.youtube.com/watch?v=5mGuCdlCcNM&t=1s"
-
-st.video(dvd, autoplay=True)
--- webseite/pages/rick_roll.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-rick_roll = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-
-st.video(rick_roll, autoplay=True)
